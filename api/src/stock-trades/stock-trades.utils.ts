53e1a65b8f028d8109bc8af93708e0fa24e31efc
Chris Chow <christian.chow.en.yi@gmail.com>
2023-12-29 12:08:14 +0800

    refactor: extract recompute functions into separate utils

---
+++ api/src/stock-trades/stock-trades.utils.ts
@@ -0,0 +1,28 @@
+import { StockTrade } from '../database/entities/stock-trade.entity'
+
+export const computeStockTradesStats = (trades: StockTrade[]) => {
+  const stats = {
+    openStocksPosition: 0,
+    realisedStocksProfit: 0,
+    numberOfOpenStockTrades: 0,
+    numberOfClosedStockTrades: 0,
+  }
+
+  for (const trade of trades) {
+    const positionMultiplier = trade.position === 'LONG' ? 1 : -1
+
+    if (trade.closePrice === undefined || trade.closePrice === null) {
+      stats.openStocksPosition -=
+        trade.openPrice * trade.quantity * positionMultiplier
+      stats.numberOfOpenStockTrades += 1
+    } else {
+      stats.realisedStocksProfit +=
+        (trade.closePrice - trade.openPrice) *
+        trade.quantity *
+        positionMultiplier
+      stats.numberOfClosedStockTrades += 1
+    }
+  }
+
+  return stats
+}
