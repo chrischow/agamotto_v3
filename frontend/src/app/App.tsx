72db675f6ab09ca769cc3a6d17fb9ea5e28b9c20
Christian Chow <christian.chow.en.yi@gmail.com>
2023-12-29 12:39:18 +0800

    feat: routes (#14)
    * build: add dependency: react-router
    
    * build: add lint-staged
    
    * feat: add route structure

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,9 +1,25 @@
 import './App.css'
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
 import Providers from './Providers'
 
 function App() {
-  return <Providers></Providers>
+  return (
+    <Providers>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={'login page'} />
+          <Route element={'navbar'}>
+            <Route path="/" element={'root (dashboard)'} />
+            <Route path="/monitor" element={'monitor all positions'} />
+            <Route path="/manage" element={'manage trades'} />
+            <Route path="*" element={'404'} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Providers>
+  )
 }
 
 export default App
