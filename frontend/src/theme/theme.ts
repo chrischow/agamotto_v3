370612c57b3ec8fccfd08a1a8443b3865780d05a
Chris Chow <christian.chow.en.yi@gmail.com>
2023-12-29 21:15:24 +0800

    fixup! feat: implement paginated table of all strategies

--- frontend/src/theme/theme.ts
+++ frontend/src/theme/theme.ts
@@ -5,5 +5,14 @@ import { extendTheme } from '@chakra-ui/react'
 export const theme = extendTheme({
   fonts: {
     brand: `'IBM Plex Sans', Inter, sans-serif`,
+    heading: `'IBM Plex Sans', Inter, sans-serif`,
+    mono: `'IBM Plex Mono', 'SFMono-Regular', Menlo, Monaco, Consolas, monospace`,
+  },
+  components: {
+    Heading: {
+      baseStyle: {
+        fontWeight: 700,
+      },
+    },
   },
 })
