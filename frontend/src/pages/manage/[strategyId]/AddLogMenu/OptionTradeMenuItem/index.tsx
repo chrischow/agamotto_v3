8b605e867647ef6f674fe48b6d360eadb8a742ba
Chris Chow <christian.chow.en.yi@gmail.com>
2023-12-31 18:13:59 +0800

    feat: implement forms to add option trades and stock trades

---
+++ frontend/src/pages/manage/[strategyId]/AddLogMenu/OptionTradeMenuItem/index.tsx
@@ -0,0 +1,86 @@
+import { Kbd, MenuItem, useDisclosure } from '@chakra-ui/react'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { parse } from 'date-fns'
+import { SubmitHandler, useForm } from 'react-hook-form'
+
+import { CreateOptionTradeDto } from '../../../../../../../api/src/dto/option-trade.dto'
+import { createOptionTrade } from '../../../../../api/optionTrades'
+import CustomModal from '../../../../../components/CustomModal'
+import OptionTradeForm from './OptionTradeForm'
+
+const OptionTradeMenuItem = ({ strategyId }: { strategyId: string }) => {
+  const queryClient = useQueryClient()
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  // Form control
+  const { register, handleSubmit, setValue, reset, getValues } =
+    useForm<CreateOptionTradeDto>({ defaultValues: { instrument: 'PUT' } })
+
+  const optionTradesMutation = useMutation({
+    mutationFn: (dto: CreateOptionTradeDto) => {
+      return createOptionTrade(strategyId, dto)
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['strategies', strategyId] })
+    },
+  })
+  const onSubmit: SubmitHandler<CreateOptionTradeDto> = (data) => {
+    optionTradesMutation.mutate(data)
+  }
+
+  return (
+    <>
+      <MenuItem onClick={onOpen}>
+        Option Trade <Kbd ml={2}>O</Kbd>
+      </MenuItem>
+      <CustomModal
+        modalSize="xl"
+        title="New Stock Trade"
+        primaryText="Create"
+        secondaryText="Cancel"
+        secondaryAction={() => {
+          onClose()
+          reset()
+        }}
+        primaryAction={() => {
+          // Fill in position
+          const quantity = getValues('quantity')
+          const position = quantity < 0 ? 'SHORT' : 'LONG'
+          setValue('quantity', Math.abs(quantity))
+          setValue('position', position)
+
+          // Format dates
+          const openDate = getValues('openDate')
+          const expiry = getValues('expiry')
+          setValue(
+            'openDate',
+            parse(openDate, 'dd/MM/yyyy', new Date()).toISOString(),
+          )
+          setValue(
+            'expiry',
+            parse(expiry, 'dd/MM/yyyy', new Date()).toISOString(),
+          )
+
+          const closeDate = getValues('closeDate')
+          if (closeDate) {
+            setValue(
+              'closeDate',
+              parse(closeDate, 'dd/MM/yyyy', new Date()).toISOString(),
+            )
+          }
+
+          // Submit
+          handleSubmit(onSubmit)()
+          reset()
+        }}
+        isOpen={isOpen}
+        onClose={onClose}
+        bodyElement={
+          <OptionTradeForm registerFn={register} setValue={setValue} />
+        }
+      />
+    </>
+  )
+}
+
+export default OptionTradeMenuItem
---
+++ frontend/src/pages/manage/[strategyId]/AddLogMenu/StockTradeMenuItem/index.tsx
@@ -0,0 +1,90 @@
+import { Kbd, MenuItem, useDisclosure } from '@chakra-ui/react'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { parse } from 'date-fns'
+import { SubmitHandler, useForm } from 'react-hook-form'
+
+import { CreateStockTradeDto } from '../../../../../../../api/src/dto/stock-trade.dto'
+import { createStockTrade } from '../../../../../api/stockTrades'
+import CustomModal from '../../../../../components/CustomModal'
+import StockTradeForm from './StockTradeForm'
+
+const StockTradeMenuItem = ({ strategyId }: { strategyId: string }) => {
+  const queryClient = useQueryClient()
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  // Form control
+  const {
+    register: stockFormRegister,
+    handleSubmit: stockFormHandleSubmit,
+    setValue: setStockFormValue,
+    reset: resetStockForm,
+    getValues: getStockFormValues,
+  } = useForm<CreateStockTradeDto>()
+
+  const stockTradesMutation = useMutation({
+    mutationFn: (dto: CreateStockTradeDto) => {
+      return createStockTrade(strategyId, dto)
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['strategies', strategyId] })
+    },
+  })
+  const onStockFormSubmit: SubmitHandler<CreateStockTradeDto> = (data) => {
+    console.log(data)
+    stockTradesMutation.mutate(data)
+  }
+
+  return (
+    <>
+      <MenuItem onClick={onOpen}>
+        Stock Trade <Kbd ml={2}>S</Kbd>
+      </MenuItem>
+      <CustomModal
+        modalSize="xl"
+        title="New Stock Trade"
+        primaryText="Create"
+        secondaryText="Cancel"
+        secondaryAction={() => {
+          onClose()
+          resetStockForm()
+        }}
+        primaryAction={() => {
+          // Fill in position
+          const quantity = getStockFormValues('quantity')
+          const position = quantity < 0 ? 'SHORT' : 'LONG'
+          setStockFormValue('quantity', Math.abs(quantity))
+          setStockFormValue('position', position)
+
+          // Format dates
+          const openDate = getStockFormValues('openDate')
+          setStockFormValue(
+            'openDate',
+            parse(openDate, 'dd/MM/yyyy', new Date()).toISOString(),
+          )
+
+          const closeDate = getStockFormValues('closeDate')
+          if (closeDate) {
+            setStockFormValue(
+              'closeDate',
+              parse(closeDate, 'dd/MM/yyyy', new Date()).toISOString(),
+            )
+          }
+
+          // Submit
+          stockFormHandleSubmit(onStockFormSubmit)()
+          resetStockForm()
+        }}
+        isOpen={isOpen}
+        onClose={onClose}
+        bodyElement={
+          <StockTradeForm
+            registerFn={stockFormRegister}
+            setValue={setStockFormValue}
+          />
+        }
+      />
+    </>
+  )
+}
+
+export default StockTradeMenuItem
