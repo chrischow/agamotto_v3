74b48b62b0643a99e9bb5767dfe6943bec603a2d
Chris Chow <christian.chow.en.yi@gmail.com>
2024-01-01 11:54:30 +0800

    feat: API utility functions to get, update, and delete option trades

--- frontend/src/api/optionTrades.ts
+++ frontend/src/api/optionTrades.ts
@@ -1,9 +1,35 @@
-import { CreateOptionTradeDto } from '../../../api/src/dto/option-trade.dto'
-import { post } from './common'
+import {
+  CreateOptionTradeDto,
+  GetOptionTradeDetailResponseDto,
+  UpdateOptionTradeDto,
+} from '../../../api/src/dto/option-trade.dto'
+import { deleteQuery, get, post, put } from './common'
 
 export const createOptionTrade = async (
   strategyId: string,
   dto: CreateOptionTradeDto,
 ) => {
   return await post(`strategies/${strategyId}/options`, dto)
+}
+
+export const getOptionTrade = async (
+  strategyId: string,
+  optionTradeId: string,
+): Promise<GetOptionTradeDetailResponseDto> => {
+  return await get(`strategies/${strategyId}/options/${optionTradeId}`)
+}
+
+export const updateOptionTrade = async (
+  strategyId: string,
+  optionTradeId: string,
+  dto: UpdateOptionTradeDto,
+) => {
+  return await put(`strategies/${strategyId}/options/${optionTradeId}`, dto)
+}
+
+export const deleteOptionTrade = async (
+  strategyId: string,
+  optionTradeId: string,
+) => {
+  return await deleteQuery(`strategies/${strategyId}/options/${optionTradeId}`)
 }
