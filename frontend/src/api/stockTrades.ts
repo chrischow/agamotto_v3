c93f7b251dd0af2e9bb51c48fa95098a8dab7a10
Chris Chow <christian.chow.en.yi@gmail.com>
2024-01-01 15:05:15 +0800

    feat: API calls to get and delete stock trade

--- frontend/src/api/stockTrades.ts
+++ frontend/src/api/stockTrades.ts
@@ -1,9 +1,26 @@
-import { CreateStockTradeDto } from '../../../api/src/dto/stock-trade.dto'
-import { post } from './common'
+import {
+  CreateStockTradeDto,
+  GetStockTradeDetailResponseDto,
+} from '../../../api/src/dto/stock-trade.dto'
+import { deleteQuery, get, post } from './common'
 
 export const createStockTrade = async (
   strategyId: string,
   dto: CreateStockTradeDto,
 ) => {
   return await post(`strategies/${strategyId}/stocks`, dto)
+}
+
+export const getStockTrade = async (
+  strategyId: string,
+  stockTradeId: string,
+): Promise<GetStockTradeDetailResponseDto> => {
+  return await get(`strategies/${strategyId}/stocks/${stockTradeId}`)
+}
+
+export const deleteStockTrade = async (
+  strategyId: string,
+  stockTradeId: string,
+) => {
+  return await deleteQuery(`strategies/${strategyId}/stocks/${stockTradeId}`)
 }
