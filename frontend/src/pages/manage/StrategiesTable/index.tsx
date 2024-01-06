f363b754eab4f319f46254c69799ba26c55ab890
Chris Chow <christian.chow.en.yi@gmail.com>
2024-01-06 20:28:51 +0800

    feat: implement sorting for strategy table

--- frontend/src/pages/manage/StrategiesTable/index.tsx
+++ frontend/src/pages/manage/StrategiesTable/index.tsx
@@ -2,6 +2,8 @@ import { useQuery } from '@tanstack/react-query'
 import {
   getCoreRowModel,
   getPaginationRowModel,
+  getSortedRowModel,
+  SortingState,
   useReactTable,
 } from '@tanstack/react-table'
 import { useEffect, useState } from 'react'
@@ -13,6 +15,7 @@ import { strategiesTableColumns } from './columnDefs'
 
 const StrategiesTable = () => {
   const [strategies, setStrategies] = useState<StrategySummaryResponse[]>([])
+  const [sorting, setSorting] = useState<SortingState>([])
 
   const { data } = useQuery({
     queryKey: ['strategies'],
@@ -28,7 +31,12 @@ const StrategiesTable = () => {
   const table = useReactTable({
     data: strategies,
     columns: strategiesTableColumns,
+    state: {
+      sorting,
+    },
+    onSortingChange: setSorting,
     getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
     getPaginationRowModel: getPaginationRowModel(),
     initialState: { pagination: { pageSize: 50 } },
   })
